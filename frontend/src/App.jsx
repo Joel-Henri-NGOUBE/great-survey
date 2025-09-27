e77d8e016730bdfd657e8016b7efd2453f5dd73c
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2025-09-27 06:12:41 +0200

    Frontend initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [form, setform] = useState({
+    username: "",
+    mail: "",
+    age: 0,
+    choice: 0
+  })
+
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App
